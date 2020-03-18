301432321b5b11cf6b71581d52a6ea8e97ccbb0a
Slava Sobolev <sobolevslava@gmail.com>
2020-03-19 00:33:08 +0100

    stats

--- src/App.js
+++ src/App.js
@@ -6,18 +6,37 @@ import StatsScreen from './components/Stats/StatsScreen';
 import QAScreen from './components/QA/QAScreen';
 import ContactsScreen from './components/Contacts/ContactsScreen';
 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import {StatsDataProvider} from './components/shared/StatsDataContext';
 
 const Tab = createBottomTabNavigator();
+
+const icons = {
+  stats: 'chart-line',
+  qa: 'cloud-question',
+  contacts: 'phone',
+};
 
 export default function App() {
   return (
     <StatsDataProvider>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Статистика" component={StatsScreen} />
-          <Tab.Screen name="QA" component={QAScreen} />
-          <Tab.Screen name="телефоны" component={ContactsScreen} />
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              const iconName = icons[route.name];
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            showLabel: false,
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}>
+          <Tab.Screen name="stats" component={StatsScreen} />
+          <Tab.Screen name="qa" component={QAScreen} />
+          <Tab.Screen name="contacts" component={ContactsScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     </StatsDataProvider>
--- src/components/Stats/StatsScreen.js
+++ src/components/Stats/StatsScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-
 import {useStatsContext} from '../shared/StatsDataContext';
+
+import styles from './StatsScreen.styles';
 
 export default function StatsScreen() {
   const {stats} = useStatsContext();
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={styles.container}>
       <Text>{JSON.stringify(stats, undefined, 2)}</Text>
     </View>
   );
