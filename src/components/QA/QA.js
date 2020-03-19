85aa7b1decb6e47dbaac76ead3419f67c406f2a2
dalyaka <idaliya.taisina@gmail.com>
2020-03-19 22:45:14 +0100

    who faq

---
+++ src/components/QA/QA.js
@@ -0,0 +1,42 @@
+import React, {useState, useCallback} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+
+import styles from './QA.styles';
+
+export function AnswerText({children}) {
+  return <Text style={styles.answerText}>{children}</Text>;
+}
+
+export function Ul({children}) {
+  return <View style={styles.ul}>{children}</View>;
+}
+
+export function Li({children}) {
+  return <View style={styles.li}>{children}</View>;
+}
+
+export function SubQuestion({children, title}) {
+  return (
+    <View style={styles.subquestion}>
+      <Text style={styles.subTitle}>{title}</Text>
+      <View style={styles.subAnswer}>{children}</View>
+    </View>
+  );
+}
+
+export function QABlock({title, children}) {
+  const [expanded, setExpanded] = useState(false);
+  const toggle = useCallback(() => setExpanded(e => !e), []);
+  return (
+    <View style={styles.block}>
+      <TouchableOpacity onPress={toggle}>
+        <Text style={styles.question}>{title}</Text>
+      </TouchableOpacity>
+      {expanded && <View style={styles.answer}>{children}</View>}
+    </View>
+  );
+}
+
+export function QA({children}) {
+  return <View style={styles.qa}>{children}</View>;
+}
