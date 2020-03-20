89201b8337759b5823a29fc1c31dd1c0d2eca01a
Slava Sobolev <sobolevslava@gmail.com>
2020-03-20 14:21:43 +0100

    arcs

--- src/components/Stats/StatsScreen.js
+++ src/components/Stats/StatsScreen.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View} from 'react-native';
+
+import Pie from './Pie';
 
 import {useStatsContext} from '../shared/StatsDataContext';
 import HeaderScrollView from '../shared/HeaderScrollView';
 
 import styles from './StatsScreen.styles';
 
+const demoData = [
+  {
+    number: 60,
+    color: '#0d2f51',
+  },
+  {
+    number: 20,
+    color: '#28BD8B',
+  },
+  {
+    number: 20,
+    color: '#F66A6A',
+  },
+];
+
 export default function StatsScreen({navigation}) {
   const {stats} = useStatsContext();
   return (
     <HeaderScrollView title="COVID-19">
-      <View style={styles.stats} />
+      <View style={styles.stats}>
+        <Pie data={demoData} width={300} height={300} />
+      </View>
     </HeaderScrollView>
   );
 }
