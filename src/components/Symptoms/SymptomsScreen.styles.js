dd1b862c8fe0b6a9003b3d9f6cee43b082751dd2
dalyaka <idaliya.taisina@gmail.com>
2020-03-22 21:29:14 +0100

    screens

--- src/components/Symptoms/SymptomsScreen.styles.js
+++ src/components/Symptoms/SymptomsScreen.styles.js
@@ -1,3 +1,12 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  scroll: {
+    backgroundColor: '#003CBF',
+    flex: 1,
+  },
+  scrollContent: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
---
+++ src/components/shared/HeaderBack.styles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  backImage: {
+    marginLeft: 24,
+    width: 24,
+    height: 24,
+  },
+});
