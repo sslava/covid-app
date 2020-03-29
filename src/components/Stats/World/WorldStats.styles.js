4b04f8c6544cf5ab58333dbcbc88df66cf5e3ebe
Slava Sobolev <sobolevslava@gmail.com>
2020-03-29 22:12:30 +0200

    stats

--- src/components/Stats/World/WorldStats.styles.js
+++ src/components/Stats/World/WorldStats.styles.js
@@ -29,8 +29,8 @@ export default StyleSheet.create({
   banner: {
     paddingTop: 8,
     paddingBottom: 24,
+    paddingHorizontal: 24,
     justifyContent: 'center',
-    alignItems: 'center',
   },
   bannerText: {
     fontFamily: 'Ubuntu',
@@ -55,5 +55,17 @@ export default StyleSheet.create({
     flex: 1,
     width: '100%',
     flexDirection: 'row',
+  },
+  updated: {
+    paddingHorizontal: 24,
+    paddingBottom: 8,
+  },
+  updatedText: {
+    fontFamily: 'Ubuntu',
+    fontSize: 14,
+    fontWeight: '300',
+    lineHeight: 20,
+    opacity: 0.7,
+    color: colors.FillText,
   },
 });
