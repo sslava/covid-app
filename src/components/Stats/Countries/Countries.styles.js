449a0195cb008bdb2404da59222b4faae9224580
Slava Sobolev <sobolevslava@gmail.com>
2020-03-30 13:29:34 +0200

    all countries

--- src/components/Stats/Countries/Countries.styles.js
+++ src/components/Stats/Countries/Countries.styles.js
@@ -15,4 +15,29 @@ export default StyleSheet.create({
     color: colors.DarkBlue,
     marginBottom: 24,
   },
+  action: {
+    paddingTop: 24,
+    paddingHorizontal: 24,
+  },
+  all: {
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    borderRadius: 5,
+    backgroundColor: 'rgba(149, 160, 184, 0.1)',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  allCaption: {
+    color: colors.DarkBlue,
+    fontFamily: 'Ubuntu',
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: '500',
+  },
+  allIcon: {
+    width: 20,
+    height: 20,
+    marginLeft: 8,
+  },
 });
