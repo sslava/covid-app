a53da51ca25b3c2845de7d68f4d4fb1a54417fa3
Slava Sobolev <sobolevslava@gmail.com>
2020-03-30 02:18:56 +0200

    resorces

---
+++ src/components/Cities/CitiesScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import styles from './CitiesScreen.styles';
+
+export default function CitiesScreen() {
+  return <View style={styles.container} />;
+}
