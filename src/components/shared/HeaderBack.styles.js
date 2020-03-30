7f7d682765bc2efdd977676aeac0e96e08ef4f29
Slava Sobolev <sobolevslava@gmail.com>
2020-03-30 02:26:36 +0200

    countries

--- src/components/shared/HeaderBack.styles.js
+++ src/components/shared/HeaderBack.styles.js
@@ -1,9 +1,11 @@
 import {StyleSheet} from 'react-native';
+import {colors} from './uikit';
 
 export default StyleSheet.create({
   backImage: {
     marginLeft: 24,
     width: 24,
     height: 24,
+    tintColor: colors.DarkBlue,
   },
 });
