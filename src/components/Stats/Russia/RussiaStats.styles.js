adfd3be655fbb572a00843db34e6c06cb52ced7d
Slava Sobolev <sobolevslava@gmail.com>
2020-03-31 02:07:22 +0200

    wc

--- src/components/Stats/Russia/RussiaStats.styles.js
+++ src/components/Stats/Russia/RussiaStats.styles.js
@@ -1,87 +1,40 @@
 import {StyleSheet} from 'react-native';
-import {legendColor, colors} from '../../shared/uikit';
 
 export default StyleSheet.create({
   container: {},
-  header: {
-    paddingHorizontal: 24,
-  },
-  title: {
-    fontFamily: 'Ubuntu',
-    fontSize: 34,
-    lineHeight: 34 * 1.2,
-    letterSpacing: 0.011,
-    fontWeight: '500',
-    color: colors.DarkBlue,
-  },
-  banner: {
-    paddingTop: 8,
-    paddingBottom: 24,
-    justifyContent: 'center',
-    paddingHorizontal: 24,
-  },
-  bannerText: {
-    fontFamily: 'Ubuntu',
-    fontSize: 14,
-    fontWeight: '600',
-    textTransform: 'uppercase',
-    color: colors.FillText,
-  },
-  bannerNumber: {
-    fontFamily: 'Ubuntu',
-    fontSize: 50,
-    fontWeight: '300',
-    lineHeight: 50 * 1.2,
-    color: colors.DarkBlue,
-  },
-  today: {
-    fontSize: 20,
-    fontWeight: '300',
-    color: legendColor.Deaths,
+  legend: {
+    marginTop: 30,
+    paddingHorizontal: 20,
   },
   bar: {
-    paddingTop: 8,
-    paddingBottom: 8,
-    paddingHorizontal: 24,
-  },
-  legend: {
-    paddingHorizontal: 24,
-  },
-  updated: {
-    paddingHorizontal: 24,
-    paddingBottom: 8,
+    marginTop: 20,
+    paddingHorizontal: 20,
   },
   updatedText: {
+    marginTop: 20,
+    paddingLeft: 20,
     fontFamily: 'Ubuntu',
-    fontSize: 14,
-    lineHeight: 20,
-    fontWeight: '300',
-    opacity: 0.7,
-    color: colors.FillText,
+    fontSize: 11,
+    fontWeight: '500',
+    lineHeight: 18,
+    color: '#8B8C9C',
   },
   action: {
-    paddingTop: 12,
-    paddingHorizontal: 24,
+    paddingTop: 20,
+    paddingHorizontal: 20,
   },
   all: {
-    paddingVertical: 16,
-    paddingHorizontal: 16,
     borderRadius: 5,
-    backgroundColor: 'rgba(149, 160, 184, 0.1)',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    height: 44,
+    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#F3F3F3',
   },
   allCaption: {
-    color: colors.DarkBlue,
+    color: '#252A34',
     fontFamily: 'Ubuntu',
     fontSize: 16,
-    lineHeight: 24,
+    lineHeight: 20,
     fontWeight: '500',
-  },
-  allIcon: {
-    width: 20,
-    height: 20,
-    marginLeft: 8,
   },
 });
--- src/components/shared/Legend/LegendItem.styles.js
+++ src/components/shared/Legend/LegendItem.styles.js
@@ -3,7 +3,6 @@ import {legendColor, colors} from '../uikit';
 
 export default StyleSheet.create({
   container: {
-    marginBottom: 8,
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
