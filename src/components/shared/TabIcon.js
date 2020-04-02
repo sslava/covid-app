1564955df9fd683ecf0abbfe612f84d8bdb094e4
Slava Sobolev <sobolevslava@gmail.com>
2020-04-02 15:28:39 +0200

    fix

--- src/components/shared/TabIcon.js
+++ src/components/shared/TabIcon.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import {Image} from 'react-native';
 
+const imgSyles = {
+  position: 'relative',
+  top: 2,
+};
+
 export default function TabIcon({focused, size, icon}) {
   const tintColor = focused ? '#087ED9' : '#A0A2AF';
   return (
     <Image
       source={icon}
-      style={{width: size + 2, height: size + 2, tintColor}}
+      style={[
+        imgSyles,
+        {
+          width: size + 1,
+          height: size + 1,
+          tintColor,
+        },
+      ]}
     />
   );
 }
