db75ba93f77ca734e86265e8076414fa76a611d4
Slava Sobolev <sobolevslava@gmail.com>
2020-04-02 15:20:23 +0200

    scrolltop on tab click

--- src/components/Info/InfoScreen.js
+++ src/components/Info/InfoScreen.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {View, ScrollView, SafeAreaView} from 'react-native';
 
 import LargeHeader from '../shared/Header/LargeHeader';
+import useTabClickScroll from '../shared/useTabClickScroll';
 import Symptoms from './Symptoms/Symptoms';
 import Recommendations from './Recommendations/Recommendations';
 import Resources from './Resources/Resources';
@@ -10,9 +11,12 @@ import Resources from './Resources/Resources';
 import styles from './InfoScreen.styles';
 
 export default function InfoScreen({navigation}) {
+  const scrollRef = useTabClickScroll();
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <ScrollView
+        ref={scrollRef}
         style={styles.scroll}
         contentContainerStyle={styles.scrollContent}>
         <LargeHeader title="Информация" />
