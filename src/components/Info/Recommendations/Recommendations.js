fb00b319cacabe4fe95c6089d394e1e5073ee100
Slava Sobolev <sobolevslava@gmail.com>
2020-04-03 16:57:02 +0200

    translate recommendations

--- src/components/Info/Recommendations/Recommendations.js
+++ src/components/Info/Recommendations/Recommendations.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useMemo} from 'react';
+import {t} from 'i18n-js';
+import {View} from 'react-native';
 
 import Recommendation from './Recommendation';
-import recommendations from './list';
-import {t} from 'i18n-js';
+import recommendationsFn from './list';
 
 import Subheader from '../Subheader';
 
 import styles from './Recommendations.styles';
 
 export default function Recommendations() {
+  const recommendations = useMemo(() => recommendationsFn(t), []);
   return (
     <View style={styles.container}>
       <Subheader>{t('info.recommentations.title')}</Subheader>
