daeeb3fab4d524cc3729a5c9d0d590c1815f213e
Slava Sobolev <sobolevslava@gmail.com>
2020-04-03 18:02:18 +0200

    translate resources

---
+++ src/components/Info/Resources/list.js
@@ -0,0 +1,41 @@
+export function getPhones(t, country) {
+  const common = [
+    {
+      dialNumber: '112',
+      phone: '112',
+      name: t('info.contacts.list.emergency'),
+    },
+  ];
+  if (country === 'Russia') {
+    return [
+      {
+        dialNumber: '+7(800)2000112',
+        phone: '8 800 2000 112',
+        name: t('info.contacts.list.ru-hotLine'),
+      },
+      ...common,
+    ];
+  }
+  return common;
+}
+
+export function getLinks(t, country) {
+  if (country === 'Russia') {
+    return [
+      {
+        title: t('info.links.list.whotitle'),
+        desc: t('info.links.list.whodesc'),
+        url:
+          'https://www.who.int/ru/emergencies/diseases/novel-coronavirus-2019/advice-for-public',
+      },
+    ];
+  }
+  return [
+    {
+      title: t('info.links.list.whotitle'),
+      desc: t('info.links.list.whodesc'),
+      url:
+        'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public',
+    },
+  ];
+}
