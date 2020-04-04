381c85a3a4001805621fb96b6180146a1977f192
Slava Sobolev <sobolevslava@gmail.com>
2020-04-04 22:22:13 +0200

    country icons

--- src/components/Stats/PrimaryCountry/PrimaryCountry.styles.js
+++ src/components/Stats/PrimaryCountry/PrimaryCountry.styles.js
@@ -20,6 +20,13 @@ export default StyleSheet.create({
     lineHeight: 18,
     color: '#8B8C9C',
   },
+  icon: {
+    height: 26,
+    width: 26,
+    borderRadius: 12,
+    marginRight: 10,
+    resizeMode: 'cover',
+  },
   numberStyle: {
     fontSize: 18,
     fontWeight: '600',
