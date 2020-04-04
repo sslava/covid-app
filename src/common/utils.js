1d0f3f0f31fd6e9bef84a6ece75764200e5637a3
Slava Sobolev <sobolevslava@gmail.com>
2020-04-04 23:35:50 +0200

    country

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,5 +1,5 @@
 export function objectSort(list, fieldGetter) {
-  return list.sort((item1, item2) => {
+  return [...list].sort((item1, item2) => {
     const a = fieldGetter(item1);
     const b = fieldGetter(item2);
 
