b0afd850aa86b7acbc0b550dcf694f684c0478b5
Slava Sobolev <sobolevslava@gmail.com>
2020-04-05 21:46:36 +0200

    useful links

--- src/components/Info/Resources/list.js
+++ src/components/Info/Resources/list.js
@@ -17,11 +17,23 @@ export const phonesMap = {
   ],
   RU: () => [
     {
-      dialNumber: '+7(800)2000112',
-      phone: '8 800 2000 112',
+      dialNumber: '+7(800)2000200',
+      phone: '8 800 200 0 200',
       name: t('info.contacts.list.ru-hotline'),
     },
+    {
+      dialNumber: '+7(800)2000112',
+      phone: '8 800 2000 112',
+      name: t('info.contacts.list.ru-stopcovid'),
+    },
   ],
+};
+
+const whoLinks = {
+  EN:
+    'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public',
+  RU:
+    'https://www.who.int/ru/emergencies/diseases/novel-coronavirus-2019/advice-for-public',
 };
 
 export const linksMap = {
@@ -29,9 +41,20 @@ export const linksMap = {
     {
       title: t('info.links.list.whotitle'),
       desc: t('info.links.list.whodesc'),
-      url:
-        'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public',
+      url: whoLinks[alpha2] || whoLinks.EN,
     },
   ],
   EN: () => [],
+  RU: () => [
+    {
+      title: t('info.links.list.minzdravtitle'),
+      desc: t('info.links.list.minzdravdesc'),
+      url: 'https://covid19.rosminzdrav.ru/',
+    },
+    {
+      title: t('info.links.list.minzdravtitle'),
+      desc: t('info.links.list.ru-regionhotline'),
+      url: 'https://rosminzdrav.ru/hotline',
+    },
+  ],
 };
