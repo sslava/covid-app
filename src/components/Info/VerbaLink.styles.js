ed9ae9170a5c4baeb2e57be5f899ba1dd5bd7e42
Slava Sobolev <sobolevslava@gmail.com>
2020-04-06 18:54:33 +0200

    verba clinic ad link

--- src/components/Info/VerbaLink.styles.js
+++ src/components/Info/VerbaLink.styles.js
@@ -4,13 +4,15 @@ import {fontStyles} from '../shared/uikit';
 export default StyleSheet.create({
   container: {
     paddingHorizontal: 25,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
   },
   text: {
+    paddingLeft: 20,
     fontFamily: 'Ubuntu',
     fontSize: 15,
     fontWeight: '600',
     lineHeight: 20,
-    textAlign: 'right',
     color: '#8B8C9C',
   },
   link: {
