e90d3b314eb54a3120f57897204b6e724ce4061c
dalyaka <idaliya.taisina@gmail.com>
2020-04-15 23:40:11 +0200

    sort by cases

--- src/components/shared/Search/useDebounceSearch.js
+++ src/components/shared/Search/useDebounceSearch.js
@@ -4,6 +4,7 @@ import {useState, useEffect} from 'react';
 export default function useDebouncedSearch<T>(
   data: Array<T>,
   filterFn: (q: string, i: T) => boolean,
+  sortFn: (arr: Array<T>) => Array<T> = (r) => r,
 ) {
   const [query, setQuery] = useState<string>('');
   const [items, setItems] = useState<Array<any>>(data);
@@ -12,11 +13,11 @@ export default function useDebouncedSearch<T>(
     const t = setTimeout(() => {
       const q = query.toLowerCase();
       const filtered = q ? data.filter((c) => filterFn(q, c)) : data;
-      setItems(filtered);
+      setItems(sortFn(filtered));
     }, 250);
 
     return () => clearTimeout(t);
-  }, [data, query, filterFn]);
+  }, [data, query, filterFn, sortFn]);
 
   return [items, query, setQuery];
 }
