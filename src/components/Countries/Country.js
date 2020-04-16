33321c249db5d516728e0f15ca3fccb56523f83e
dalyaka <idaliya.taisina@gmail.com>
2020-04-16 17:28:23 +0200

    country filter -> disaply numbers

--- src/components/Countries/Country.js
+++ src/components/Countries/Country.js
@@ -8,16 +8,29 @@ import RegionListLegend from '../shared/RegionList/RegionListLegend';
 
 import {countryName} from '../../common/locale';
 
-function Country({country}) {
+const SORT_ACTIVE = 1;
+const SORT_DEATHS = 2;
+
+function getNumbers(country, sort) {
+  switch (sort) {
+    case SORT_ACTIVE:
+      return {total: country.active, today: country.total_new};
+    case SORT_DEATHS:
+      return {total: country.deaths, today: country.deaths_new, deaths: true};
+    default:
+      break;
+  }
+  return {total: country.total, today: country.total_new};
+}
+
+function Country({country, sort = 0}) {
   const [expanded, setExpanded] = useState(false);
   const toggle = useCallback(() => setExpanded((s) => !s), []);
-
   return (
     <RegionListItem expanded={expanded}>
       <RegionListToggle
         name={countryName(country)}
-        total={country.total}
-        today={country.total_new}
+        {...getNumbers(country, sort)}
         expanded={expanded}
         onPress={toggle}
       />
