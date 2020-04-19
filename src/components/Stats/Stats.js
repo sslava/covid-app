51a37c8738e61b1dd78e1fa2709d72bef0852af0
Slava Sobolev <sobolevslava@gmail.com>
2020-04-19 16:23:42 +0200

    redux

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -4,8 +4,8 @@ import {useSelector} from 'react-redux';
 import {t} from '../../common/locale';
 import {
   topCountriesSelector,
-  countriesSelector,
   worldSelector,
+  makeCountrySelector,
 } from '../../app/statsModule';
 
 import LargeHeader from '../shared/Header/LargeHeader';
@@ -20,26 +20,12 @@ import {
   CountriesContainer,
 } from './Stats.styles';
 
-function findPrimary(countries, code) {
-  if (!code) {
-    return null;
-  }
-  const primary = countries.find((c) => c.code === code);
-  if (!primary) {
-    return countries.find((c) => c.code === 'US');
-  }
-  return primary;
-}
-
 export default function Stats({prefs}) {
-  const countries = useSelector(countriesSelector);
+  const primarySelector = useMemo(makeCountrySelector);
+  const primary = useSelector((s) => primarySelector(s, prefs.primary));
+
   const top = useSelector(topCountriesSelector);
   const world = useSelector(worldSelector);
-
-  const primary = useMemo(() => findPrimary(countries, prefs.primary), [
-    countries,
-    prefs.primary,
-  ]);
 
   return (
     <Container>
