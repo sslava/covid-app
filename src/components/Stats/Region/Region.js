89d0607e115dc03ab9d48711490baf821dd62f07
Slava Sobolev <sobolevslava@gmail.com>
2020-04-24 22:55:15 +0200

    regions

---
+++ src/components/Stats/Region/Region.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import {t} from '../../../common/locale';
+
+import Subheader from '../common/Subheader';
+
+import ddIcon from '../../../assets/icons/dropdown.png';
+import markerIcon from '../../../assets/icons/marker.png';
+
+import {ShareIcon, ListIcon} from '../../shared/buttonIcons';
+
+import {
+  Container,
+  Content,
+  Marker,
+  DDIcon,
+  Actions,
+  ShareBtn,
+  CountryBtn,
+} from './Region.styles';
+
+export default function Region({code}) {
+  const name = 'Ханты-Мансийский АО';
+  return (
+    <Container>
+      <Subheader
+        title={name}
+        icon={<Marker source={markerIcon} />}
+        // onPress={changeCountry}
+        activeOpacity={0.7}>
+        <DDIcon source={ddIcon} />
+      </Subheader>
+      <Content>
+        <Actions>
+          <ShareBtn icon={<ShareIcon />}>{t('stats.country.share')}</ShareBtn>
+          <CountryBtn icon={<ListIcon />}>{t('stats.regions.all')}</CountryBtn>
+        </Actions>
+      </Content>
+    </Container>
+  );
+}
