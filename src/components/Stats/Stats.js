e179078d46c9a68e446bc22177d5a00c5c9d2384
dalyaka <idaliya.taisina@gmail.com>
2020-04-24 22:55:15 +0200

    regions

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -6,13 +6,17 @@ import LargeHeader from '../shared/Header/LargeHeader';
 import WorldStats from './World/WorldStats';
 import PrimaryCountry from './PrimaryCountry/PrimaryCountry';
 import Countries from './Countries/Countries';
+import Region from './Region/Region';
 
 import {
   Container,
   WorldContainer,
   PrimaryContainer,
   CountriesContainer,
+  RegionContainer,
 } from './Stats.styles';
+
+const hasRegion = (code) => code === 'US' || code === 'RU';
 
 export default function Stats({prefs}) {
   return (
@@ -24,6 +28,11 @@ export default function Stats({prefs}) {
       <PrimaryContainer>
         <PrimaryCountry code={prefs.primary} />
       </PrimaryContainer>
+      {hasRegion(prefs.primary) && (
+        <RegionContainer>
+          <Region code={prefs.primary} />
+        </RegionContainer>
+      )}
       <CountriesContainer>
         <Countries />
       </CountriesContainer>
