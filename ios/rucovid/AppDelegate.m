57a363e377d9ca1aeb246c29d158dd041df0d25d
Slava Sobolev <sobolevslava@gmail.com>
2020-04-28 15:06:07 +0200

    splash screen

--- ios/rucovid/AppDelegate.m
+++ ios/rucovid/AppDelegate.m
@@ -36,13 +36,18 @@ static void InitializeFlipper(UIApplication *application) {
                                                    moduleName:@"rucovid"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed: 0 green: 122/255 blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  UIView* launchScreenView = [[[NSBundle mainBundle] loadNibNamed:@"LaunchScreen" owner:self options:nil] objectAtIndex:0];
+  launchScreenView.frame = self.window.bounds;
+  rootView.loadingView = launchScreenView;
+  
   return YES;
 }
 
