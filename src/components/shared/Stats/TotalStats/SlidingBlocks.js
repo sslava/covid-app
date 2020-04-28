34a6df30cd11845780f41e79c6d0ba957ec61107
dalyaka <idaliya.taisina@gmail.com>
2020-04-28 18:24:29 +0200

    total stats

--- src/components/shared/Stats/TotalStats/SlidingBlocks.js
+++ src/components/shared/Stats/TotalStats/SlidingBlocks.js
@@ -1,23 +1,6 @@
 import React from 'react';
-import styled from 'styled-components/native';
 
-const Container = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const Growing = styled.View`
-  margin-right: 5px;
-  flex-grow: ${(p) => p.fraction};
-`;
-
-const Left = styled.View`
-  flex-direction: row;
-  flex-grow: 1;
-  padding-right: 3px;
-`;
-
-export const Right = styled.View``;
+import {Container, Growing, Left, Right} from './SlidingBlocks.styles';
 
 export default function SlidingBlocks({left, right, center, fraction}) {
   return (
