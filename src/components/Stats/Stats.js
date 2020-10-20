ec9c27226a50234d35f6bb690df7700ea5f2e3e2
Slava Sobolev <sobolevslava@gmail.com>
2020-10-21 00:04:35 +0200

    fix adv

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -36,7 +36,7 @@ export default function Stats({code}) {
       <WorldContainer>
         <WorldStats />
       </WorldContainer>
-      <AdmobBlock palce="statsMiddle" />
+      <AdmobBlock place="statsMiddle" />
       <CountriesContainer>
         <Countries />
       </CountriesContainer>
